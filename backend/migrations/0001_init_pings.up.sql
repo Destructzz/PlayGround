1fd3c9ada52c0edee28022c512da5626a2a8e689
Destructzz <sklyarenko_2006@mail.ru>
2026-01-20 22:54:45 +0300

    feat: add test migrations

--- backend/migrations/0001_init_pings.up.sql
+++ backend/migrations/0001_init_pings.up.sql
@@ -1,9 +1,6 @@
--- +goose Up
 CREATE TABLE IF NOT EXISTS pings (
     id BIGSERIAL PRIMARY KEY,
     content TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
--- +goose Down
-DROP TABLE IF EXISTS pings;
