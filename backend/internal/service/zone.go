7a3c1321fd324173b1b5b3dba242696dc7b00687
Destructzz <sklyarenko_2006@mail.ru>
2026-02-08 17:45:27 +0300

    update spacing

--- backend/internal/service/zone.go
+++ backend/internal/service/zone.go
@@ -10,24 +10,32 @@ import (
 )
 
 type ZoneService struct {
-    queries *sqlc.Queries
+	queries *sqlc.Queries
 }
 
 func NewZone(queries *sqlc.Queries) *ZoneService {
-    return &ZoneService{queries: queries}
+	return &ZoneService{queries: queries}
 }
 
-func (z* ZoneService) CreateZone(ctx context.Context, dto domain.CreateZoneRequest) (sqlc.Zone, error){
+func (z *ZoneService) CreateZone(ctx context.Context, dto domain.CreateZoneRequest) (sqlc.Zone, error) {
 	return z.queries.CreateZone(
 		ctx,
 		sqlc.CreateZoneParams{
-			Name: dto.Name,
+			Name:     dto.Name,
 			ZoneType: dto.Type,
 			Capacity: int32(dto.Capacity),
 			Description: pgtype.Text{
 				String: dto.Description,
-				Valid: dto.Description != "",
+				Valid:  dto.Description != "",
 			},
 		},
 	)
+}
+
+func (z *ZoneService) GetZones(ctx context.Context) ([]sqlc.Zone, error) {
+	return z.queries.ListZones(ctx)
+}
+
+func (z *ZoneService) GetZoneByID(ctx context.Context, id int64) (sqlc.Zone, error) {
+	return z.queries.GetZoneByID(ctx, id)
 }
